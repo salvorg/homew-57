a300733620672e77bb235d0c47f3987ba87de4bf
Almaz Balbaev <kujitengu@gmail.com>
2022-11-11 18:47:42 +0600

    finished. App is working, but not correct

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {User} from "./types";
+import UserForm from "./components/UserForm/UserForm";
+import Users from "./components/Users/Users";
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  const addUser = (newUser: User) => {
+    setUsers(prev => [...prev, newUser]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container-fluid">
+        <div className="row mt-2">
+          <div className="col">
+            <UserForm onSubmit={addUser}/>
+          </div>
+          <div className="col">
+            <Users users={users}/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
